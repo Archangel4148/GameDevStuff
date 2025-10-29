31e57b94848782ffba90276d84a37602da313c15
Simon Edmunds <sjebkc@umsystem.edu>
2025-10-29 11:34:21 -0500

    Finishing demo

--- colors/scenes/game.gd
+++ colors/scenes/game.gd
@@ -1,11 +1,43 @@
 extends Node2D
 
+var can_be_paused = false
+var is_paused = false
 
 func _ready() -> void:
 	# Pause everything on start, and show the menu
 	get_tree().paused = true
 	$"UI/Main Menu".show()
+	
+	# Hide the pause menu 
+	$"UI/Pause Menu".hide()
 
+func _input(event):
+	if event.is_action_pressed("pause"):
+		pause_unpause()
 
 func _on_main_menu_start_game() -> void:
 	get_tree().paused = false
+	can_be_paused = true
+
+func pause_unpause() -> void:
+	if not can_be_paused:
+		return
+	if is_paused:
+		# Unpause the game and hide the pause menu
+		get_tree().paused = false
+		$"UI/Pause Menu".hide()
+		is_paused = false
+	else:
+		# Pause the game and display the pause menu
+		get_tree().paused = true
+		$"UI/Pause Menu".show_pause_menu()
+		is_paused = true
+
+func _on_pause_menu_resume_game() -> void:
+	pause_unpause()
+
+func _on_music_volume_slider_value_changed(value: float) -> void:
+	MusicManager.volume_linear = value / 100
+
+func _on_sfx_volume_slider_value_changed(value: float) -> void:
+	$"Color Boi/Sound Effect Player".volume_linear = value / 100
--- colors/scripts/level_1.gd
+++ colors/scripts/level_1.gd
@@ -1,19 +1,34 @@
 extends Node2D
 
+var tutorial_zone_triggered = false
+var double_jump_triggered = false
+var finish_triggered = false
+var leap_of_faith_triggered = false
+
 func _ready() -> void:
 	# Start the background music
 	MusicManager.play_track("res://assets/music/level_1_music.wav")
 
-func fade_to_next_text():
-	# Fade out current text
-	var tween = create_tween()
-	tween.tween_property($"Background/Swap Colors Text", "modulate:a", 0, 0.5)  # fade out
-	tween.tween_property($"Background/Matching Colors Text", "modulate:a", 1, 0.5)  # fade in
-
 func _on_tutorial_next_trigger_body_entered(_body: Node2D) -> void:
-	fade_to_next_text()
+	if not tutorial_zone_triggered:
+		var tween = create_tween()
+		tween.tween_property($"Background/Swap Colors Text", "modulate:a", 0, 0.5)  # fade out
+		tween.tween_property($"Background/Matching Colors Text", "modulate:a", 1, 0.5)  # fade in
+		tutorial_zone_triggered = true
 
 func _on_finish_trigger_body_entered(_body: Node2D) -> void:
-	MusicManager._fade_out_then_play("res://assets/sound_effects/victory_sound.wav", 0.5)
+	if not finish_triggered:
+		MusicManager._fade_out_then_play("res://assets/sound_effects/victory_sound.wav", 0.5)
+		var tween = create_tween()
+		tween.tween_property($"Background/Finish Label", "modulate:a", 1, 0.5)  # fade in
+		finish_triggered = true
+
+
+func _on_leap_of_faith_trigger_body_entered(_body: Node2D) -> void:
 	var tween = create_tween()
-	tween.tween_property($"Background/Finish Label", "modulate:a", 1, 0.5)  # fade in
+	tween.tween_property($"Background/Leap of Faith Label", "modulate:a", 1, 0.5)  # fade in
+
+
+func _on_double_jump_trigger_body_entered(_body: Node2D) -> void:
+	var tween = create_tween()
+	tween.tween_property($"Background/Double Jump Label", "modulate:a", 1, 0.5)  # fade in
